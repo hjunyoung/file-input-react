6aa69c76bc2352461fbd8b718188ea8eece3d7c6
Junyoung Hong <hjunyoung10@gmail.com>
2021-10-19 19:06:56 +0900

    🎉 Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from './GlobalStyle';
+import Uploader from './Uploader';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Uploader />
+    </>
+  );
+}
+
+export default App;
